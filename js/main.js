261e6277e20323455853f490d11183676a6c6568
delosait-ru <info@delosait.ru>
2022-03-30 12:51:02 +0300

    2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
+"use strict";
+
+console.log('global');
+"use strict";
+
 $(function () {
+  'use strict';
 
-    'use strict';
-
-    $('.item').first().addClass('active');
-
-    $('.info').first().show().animate({
-        width: '70%'
+  $('.item').first().addClass('active');
+  $('.info').first().show().animate({
+    width: '70%'
+  });
+  $('.item').click(function () {
+    $(this).addClass("active").siblings('.item').removeClass('active');
+    $(this).next().show().animate({
+      width: '70%'
+    }).siblings('.info').animate({
+      width: 0
+    }, function () {
+      $(this).hide();
     });
-
-    $('.item').click(function () {
-
-        $(this).addClass("active").siblings('.item').removeClass('active');
-
-        $(this).next().show().animate({
-            width: '70%'
-        }).siblings('.info').animate({
-            width: 0
-        }, function () {
-
-            $(this).hide();
-
-        });
-
-    });
-
+  });
 });
+//# sourceMappingURL=main.js.map
